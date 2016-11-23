4be2fc6e6330d6b8ffca5a3f40f1483a81b8bdc2
3pings <jbarksdale@threepings.com>
2016-11-23 09:44:07 -0500

    Changed exposed port to 5000

--- chive_web/chive_web.py
+++ chive_web/chive_web.py
@@ -28,7 +28,5 @@ def index():
         print "Problem getting data...trying again..."
         pass
 
-# !flask/bin/python
-# app.run(debug=True, host='0.0.0.0', port=5000)
 
-app.run(debug=True, host='0.0.0.0', port=80)
+app.run(debug=True, host='0.0.0.0', port=5000)
