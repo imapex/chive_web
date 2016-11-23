a76cb9687fb2ab1cddafc0e4bd4cae57f11887c3
3pings <jbarksdale@threepings.com>
2016-11-23 07:58:12 -0500

    Moved folders and added Dockerfile

--- chive_web/chive_web.py
+++ chive_web/chive_web.py
@@ -6,6 +6,7 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/info')
 def index():
@@ -28,6 +29,6 @@ def index():
         pass
 
 # !flask/bin/python
-#app.run(debug=True, host='0.0.0.0', port=5000)
+# app.run(debug=True, host='0.0.0.0', port=5000)
 
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0')
